8528d852ac169f52dfc636478f3b9a5b79d01bcf
khanshadab1998 <khanshadab121998@gmail.com>
2024-03-17 12:18:34 -0400

    added Glide module, candiate adapter class to fetch data and display it on the recycler view of 'candidate_activity.xml'  using the card view layout of 'candidate_layout' made some changes on view ids to work with the adapter class

---
+++ app/src/main/java/com/example/groupproject_linkup/Candidate.kt
@@ -0,0 +1,13 @@
+package com.example.groupproject_linkup
+
+data class Candidate(var candidateName: String="",
+                     var description:String="",
+                     var address:String="",
+                     var email:String="",
+                     var experience:String="",
+                     var image: String="",
+                     var postdesc: String="",
+                     var postImg: String="",
+                     var postName: String="",
+                     var title: String="" )
+
